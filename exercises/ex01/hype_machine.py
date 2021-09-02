7c9a1b02aadbc32c39ccbcbc8ce2b5ecfbeb7a46
kdflack <kdflack@live.unc.edu>
2021-09-02 12:00:58 -0400

    Initial

--- exercises/ex01/hype_machine.py
+++ exercises/ex01/hype_machine.py
@@ -1,5 +1,12 @@
 # TODO: Write docstring here
-print("kll")
+"""Name Program"""
 # TODO: Initialize __author__ variable here
-
+__author__ = "730385030"
 # TODO: Implement your program logic here
+
+name: str = input("What is your name? ")
+print("You entered: ")
+print(name)
+print(name + ", you are cool.")
+print("You're going to have a great day, " + name + "!")
+print("You know what " + name + " you are awesome!")
